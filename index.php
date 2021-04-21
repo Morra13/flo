3d01d4ef5b1a28eb426b5456695fb89fa6835166
yuriy lisov <lisov1988@gmail.com>
2021-04-21 14:36:52 +0300

    add side-menu

---
+++ index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE HTML>
+<head>
+    <title>Flojet</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'>
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+    <script type="text/javascript" src="js/jquery-1.9.0.min.js"></script>
+    <script src="js/jquery.openCarousel.js" type="text/javascript"></script>
+    <script type="text/javascript" src="js/easing.js"></script>
+    <script type="text/javascript" src="js/move-top.js"></script>
+</head>
+<body>
+<div class="header">
+    <div class="wrap">
+        <div class="header_top">
+            <div class="logo">
+                <a href="index.html"><img src="images/logo.png" alt=""/></a>
+            </div>
+            <div class="header_top_right">
+                <div class="search_box">
+                    <span>Search</span>
+                    <form>
+                        <input type="text" value=""><input type="submit" value="">
+                    </form>
+                    <div class="clear"></div>
+                </div>
+            </div>
+            <div class="clear"></div>
+        </div>
+        <div class="navigation">
+            <!--            <a class="toggleMenu" href="#">Menu</a>-->
+            <?php require_once 'topMenu.php'?>
+        </div>
+        <div class="header_bottom">
+            <img src="images/banner_1.jpg" alt=""/>
+
+            <!--            <div class="slider-text">-->
+            <!--                <h2>Lorem Ipsum Placerat <br/>Elementum Quistue Tunulla Maris</h2>-->
+            <!--                <p>Vivamus vitae augue at quam frigilla tristique sit amet<br/> acin ante sikumpre tisdin.</p>-->
+            <!--                <a href="#">Sitamet Tortorions</a>-->
+            <!--            </div>-->
+            <!--            <div class="slider-img">-->
+            <!--                <img src="images/slider-img.png" alt=""/>-->
+            <!--            </div>-->
+            <div class="clear"></div>
+        </div>
+    </div>
+</div>
+<!------------End Header ------------>
+<div class="main">
+    <div class="content">
+        <div class="content_bottom">
+            <div class="wrap">
+                <div class="content-bottom-left">
+                    <div class="categories">
+                        <ul>
+                            <h3>Все товары</h3>
+                            <li><a href="#">Категории</a></li>
+                            <li><a href="#">Flojet</a></li>
+                            <li><a href="#">Jabsco</a></li>
+                            <li><a href="#">Singflo</a></li>
+                            <li><a href="#">Max Flow</a></li>
+                            <li><a href="#">Vitop</a></li>
+                            <li><a href="#">Насосы ЭЦВ</a></li>
+                            <li><a href="#">Sales</a></li>
+                            <li><a href="#">Справочный матерал</a></li>
+                        </ul>
+                    </div>
+                    <div class="buters-guide">
+                        <h3>Карта сайта</h3>
+                        <p><span>Калькулятор</span></p>
+                        <p> -> Сюда вфигачить калькулятор <- </p>
+                    </div>
+                    <div class="add-banner">
+                        <img src="images/camera.png" alt=""/>
+                        <div class="banner-desc">
+                            <h4>Electronics</h4>
+                            <a href="#">More Info</a>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+                    <div class="add-banner add-banner2">
+                        <img src="images/computer.png" alt=""/>
+                        <div class="banner-desc">
+                            <h4>Computers</h4>
+                            <a href="#">More Info</a>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+
+                <div class="content-bottom-right">
+                    <h3>По идее навигатор</h3>
+                    <div class="section group">
+                        <div class="grid_1_of_4 images_1_of_4">
+                            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+                            <a href="preview.html"><img src="images/product-img1.jpg" alt=""/></a>
+                            <div class="price-details">
+                                <div class="price-number">
+                                    <p><span class="rupees">$839.93 </span></p>
+                                </div>
+                                <div class="add-cart">
+                                    <h4><a href="preview.html">More Info</a></h4>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                        <div class="grid_1_of_4 images_1_of_4">
+                            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+                            <a href="preview.html"><img src="images/product-img2.jpg" alt=""/></a>
+                            <div class="price-details">
+                                <div class="price-number">
+                                    <p><span class="rupees">$839.93 </span></p>
+                                </div>
+                                <div class="add-cart">
+                                    <h4><a href="preview.html">More Info</a></h4>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+
+                        </div>
+                        <div class="grid_1_of_4 images_1_of_4">
+                            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+                            <a href="preview.html"><img src="images/product-img3.jpg" alt=""/></a>
+                            <div class="price-details">
+                                <div class="price-number">
+                                    <p><span class="rupees">$839.93 </span></p>
+                                </div>
+                                <div class="add-cart">
+                                    <h4><a href="preview.html">More Info</a></h4>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+
+                        </div>
+                        <div class="grid_1_of_4 images_1_of_4">
+                            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+                            <a href="preview.html"><img src="images/product-img4.jpg" alt=""/></a>
+                            <div class="price-details">
+                                <div class="price-number">
+                                    <p><span class="rupees">$839.93 </span></p>
+                                </div>
+                                <div class="add-cart">
+                                    <h4><a href="preview.html">More Info</a></h4>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="section group">
+                        <div class="grid_1_of_4 images_1_of_4">
+                            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+                            <a href="preview.html"><img src="images/product-img1.jpg" alt=""/></a>
+                            <div class="price-details">
+                                <div class="price-number">
+                                    <p><span class="rupees">$839.93 </span></p>
+                                </div>
+                                <div class="add-cart">
+                                    <h4><a href="preview.html">More Info</a></h4>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                        <div class="grid_1_of_4 images_1_of_4">
+                            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+                            <a href="preview.html"><img src="images/product-img2.jpg" alt=""/></a>
+                            <div class="price-details">
+                                <div class="price-number">
+                                    <p><span class="rupees">$839.93 </span></p>
+                                </div>
+                                <div class="add-cart">
+                                    <h4><a href="preview.html">More Info</a></h4>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+
+                        </div>
+                        <div class="grid_1_of_4 images_1_of_4">
+                            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+                            <a href="preview.html"><img src="images/product-img3.jpg" alt=""/></a>
+                            <div class="price-details">
+                                <div class="price-number">
+                                    <p><span class="rupees">$839.93 </span></p>
+                                </div>
+                                <div class="add-cart">
+                                    <h4><a href="preview.html">More Info</a></h4>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+
+                        </div>
+                        <div class="grid_1_of_4 images_1_of_4">
+                            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+                            <a href="preview.html"><img src="images/product-img4.jpg" alt=""/></a>
+                            <div class="price-details">
+                                <div class="price-number">
+                                    <p><span class="rupees">$839.93 </span></p>
+                                </div>
+                                <div class="add-cart">
+                                    <h4><a href="preview.html">More Info</a></h4>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="section group">
+                        <div class="grid_1_of_4 images_1_of_4">
+                            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+                            <a href="preview.html"><img src="images/product-img1.jpg" alt=""/></a>
+                            <div class="price-details">
+                                <div class="price-number">
+                                    <p><span class="rupees">$839.93 </span></p>
+                                </div>
+                                <div class="add-cart">
+                                    <h4><a href="preview.html">More Info</a></h4>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                        <div class="grid_1_of_4 images_1_of_4">
+                            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+                            <a href="preview.html"><img src="images/product-img2.jpg" alt=""/></a>
+                            <div class="price-details">
+                                <div class="price-number">
+                                    <p><span class="rupees">$839.93 </span></p>
+                                </div>
+                                <div class="add-cart">
+                                    <h4><a href="preview.html">More Info</a></h4>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+
+                        </div>
+                        <div class="grid_1_of_4 images_1_of_4">
+                            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+                            <a href="preview.html"><img src="images/product-img3.jpg" alt=""/></a>
+                            <div class="price-details">
+                                <div class="price-number">
+                                    <p><span class="rupees">$839.93 </span></p>
+                                </div>
+                                <div class="add-cart">
+                                    <h4><a href="preview.html">More Info</a></h4>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+
+                        </div>
+                        <div class="grid_1_of_4 images_1_of_4">
+                            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+                            <a href="preview.html"><img src="images/product-img4.jpg" alt=""/></a>
+                            <div class="price-details">
+                                <div class="price-number">
+                                    <p><span class="rupees">$839.93 </span></p>
+                                </div>
+                                <div class="add-cart">
+                                    <h4><a href="preview.html">More Info</a></h4>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="footer">
+    <div class="wrap">
+        <div class="copy_right">
+            <p>Телефон для связи: +7 (985) 308 6656, 8 (977) 166 0091</p>
+        </div>
+        <div class="footer-nav">
+            <ul>
+                <!--LiveInternet counter  Добавили мы для проверки посещения    -->
+                <script type="text/javascript"><!--
+                    document.write("<a href='http://www.liveinternet.ru/click' " +
+                        "target=_blank><img src='http://counter.yadro.ru/hit?t14.6;r" +
+                        escape(document.referrer) + ((typeof (screen) == "undefined") ? "" :
+                            ";s" + screen.width + "*" + screen.height + "*" + (screen.colorDepth ?
+                            screen.colorDepth : screen.pixelDepth)) + ";u" + escape(document.URL) +
+                        ";" + Math.random() +
+                        "' alt='' title='LiveInternet: показано просмотров за последние 24 часа, посетителей за сутки и за сегодняшний день ' " +
+                        "border=0 width=88 height=31><\/a>")//--></script>
+                <!--/LiveInternet-->
+                <li><a href="#">Terms of Use</a> :</li>
+                <li><a href="#">Privacy Policy</a> :</li>
+                <li><a href="contact.html">Contact Us</a> :</li>
+                <li><a href="#">Sitemap</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $().UItoTop({easingType: 'easeOutQuart'});
+
+    });
+</script>
+<a href="#" id="toTop"> </a>
+<script type="text/javascript" src="js/navigation.js"></script>
+</body>
+</html>
+
+
---
+++ topMenu.php
@@ -0,0 +1,47 @@
+<ul class="nav">
+    <li>
+        <a href="index.html">Главная</a>
+    </li>
+    <li>
+        <a href="#">Flojet</a>
+    </li>
+    <li>
+        <a href="#">Jabsco</a>
+    </li>
+    <li>
+        <a href="#">Singflo</a>
+    </li>
+    <li>
+        <a href="#">VITOP</a>
+    </li>
+    <li>
+        <a href="#">Информация</a>
+        <ul>
+            <li>
+                <a href="#">Доставка</a>
+            </li>
+            <li>
+                <a href="#">Гарантия</a>
+            </li>
+            <li>
+                <a href="#">Состояние заказа</a>
+            </li>
+            <li>
+                <a href="#">FAQ</a>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <a href="#">Контакты</a>
+        <ul>
+            <li>
+                <a href="#">Написать письмо</a>
+            </li>
+            <li>
+                <a href="#">Обратный звонок</a>
+            </li>
+        </ul>
+    </li>
+</ul>
+<span class="left-ribbon"> </span>
+<span class="right-ribbon"> </span>
